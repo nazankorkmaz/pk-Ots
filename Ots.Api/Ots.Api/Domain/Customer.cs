28f247ff96b97fc768303823e64f7c65af8b6aee
Nazan Korkmaz <nazzankorkmazz@gmail.com>
2025-03-18 23:34:08 +0300

    database baglantilari eklendi ve migrationslar olusturuldu.

--- Ots.Api/Ots.Api/Domain/Customer.cs
+++ Ots.Api/Ots.Api/Domain/Customer.cs
@@ -1,33 +1,30 @@
-namespace Ots.Api.Domain;
-
 using System.ComponentModel.DataAnnotations.Schema;
-using Ots.Base;
-
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ots.Base;
 
+namespace Ots.Api.Domain;
 
 [Table("Customer", Schema = "dbo")]
-public class Customer: BaseEntity
+public class Customer : BaseEntity
 {
-
-    public string Name {get;set;}
-    public String Email {get;set;}
-    public String Phone {get;set;}
+    public string Email { get; set; }
+    public string FirstName { get; set; }
+    public string? MiddleName { get; set; }
+    public string LastName { get; set; }
+    public string IdentityNumber { get; set; }
     public int CustomerNumber { get; set; }
-
     public DateTime OpenDate { get; set; }
-    public virtual List<CustomerAddress> CustomerAdresses {get;set;} // yani bir customer altında birden fazla adres olabilir birden coka iliski
-
-    //public virtual List<CustomerPhone> CustomerPhones { get; set; }
+    public virtual List<CustomerAddress> CustomerAddresses { get; set; }
+    public virtual List<CustomerPhone> CustomerPhones { get; set; }
     public virtual List<Account> Accounts { get; set; }
 }
 
-public class CustomerConfiguration : IEntityTypeConfiguration<Customer>{
-
+public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+{
     public void Configure(EntityTypeBuilder<Customer> builder)
     {
-builder.HasKey(x => x.Id);
+        builder.HasKey(x => x.Id);
         builder.Property(x => x.Id).UseIdentityColumn();
 
         builder.Property(x=> x.InsertedDate).IsRequired(true);
@@ -37,28 +34,26 @@ builder.HasKey(x => x.Id);
         builder.Property(x=> x.IsActive).IsRequired(true).HasDefaultValue(true);
 
         builder.Property(x => x.Email).IsRequired().HasMaxLength(100);
-/*
         builder.Property(x => x.FirstName).IsRequired().HasMaxLength(100);
         builder.Property(x => x.MiddleName).IsRequired(false).HasMaxLength(100);
         builder.Property(x => x.LastName).IsRequired().HasMaxLength(100);
         builder.Property(x => x.IdentityNumber).IsRequired().HasMaxLength(11);
-        */
         builder.Property(x => x.CustomerNumber).IsRequired();
         builder.Property(x => x.OpenDate).IsRequired(true);
-/*
-        builder.HasMany(x => x.CustomerAddresses)
+
+        //public virtual List<CustomerAddress> CustomerAdresses {get;set;} // yani bir customer altında birden fazla adres olabilir birden coka iliski
+        builder.HasMany(x => x.CustomerAddresses)  // 
             .WithOne(x => x.Customer)
             .HasForeignKey(x => x.CustomerId).IsRequired(true).OnDelete(DeleteBehavior.Cascade);
-*/
+
         builder.HasMany(x => x.Accounts)
             .WithOne(x => x.Customer)
             .HasForeignKey(x => x.CustomerId).IsRequired(true).OnDelete(DeleteBehavior.Cascade);
 
-       /* builder.HasMany(x => x.CustomerPhones)
+        builder.HasMany(x => x.CustomerPhones)
             .WithOne(x => x.Customer)
             .HasForeignKey(x => x.CustomerId).IsRequired(true).OnDelete(DeleteBehavior.Cascade);
-*/
-        builder.HasIndex(x => x.CustomerNumber).IsUnique(true); // veritabani constrainti yani orada unique olmasini sagliyor
+
+        builder.HasIndex(x => x.CustomerNumber).IsUnique(true); //// veritabani constrainti yani orada unique olmasini sagliyor
     }
 }
-
--- Ots.Api/Ots.Api/Startup.cs
+++ Ots.Api/Ots.Api/Startup.cs
@@ -1,26 +1,44 @@
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+//using Ots.Api.Impl.Cqrs;
+
 namespace Ots.Api;
 
-public class Startup{
+public class Startup
+{
+    public IConfiguration Configuration { get; }
+    public Startup(IConfiguration configuration) => Configuration = configuration;
+
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddControllers();
+
         services.AddSwaggerGen();
+
+        string connectionStringMsSql = Configuration.GetConnectionString("MsSqlConnection");
+        string connectionStringPostgresql = Configuration.GetConnectionString("PostgreSqlConnection");
+
+        services.AddDbContext<OtsMsSqlDbContext>(options => { options.UseSqlServer(connectionStringMsSql); });
+        services.AddDbContext<OtsPostgreSqlDbContext>(options => { options.UseNpgsql(connectionStringPostgresql); });
+
+        //services.AddMediatR(x=> x.RegisterServicesFromAssemblies(typeof(CreateCustomerCommand).GetTypeInfo().Assembly));
     }
 
-    public void Configure(IApplicationBuilder app,IWebHostEnvironment env)
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
         if (env.IsDevelopment())
         {
             app.UseDeveloperExceptionPage();
             app.UseSwagger();
             app.UseSwaggerUI();
-
         }
 
         app.UseHttpsRedirection();
+
         app.UseRouting();
+
         app.UseAuthorization();
+
         app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
     }
-    
 }
